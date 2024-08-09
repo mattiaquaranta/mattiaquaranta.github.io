9e5b267651b3e1b2a3c23708dee0c3da54a7fe06
mattia <mattiaquaranta15@gmail.com>
2024-08-09 12:23:23 +0200

    ultime rifiniture responsive

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,11 +1,18 @@
 function sendMail() {
-    var params = {
-        from_name : document.getElementById("full-name").value,
-        email_id : document.getElementById("email").value,
-        object : document.getElementById("object").value,
-        message : document.getElementById("message").value
-    }
-
-    emailjs.send("service_tovtr19", "template_s0f2f17", params)
-        .then(alert("Messaggio inviato correttamente"));
+    if ((document.getElementById("full-name").value == "") || 
+    (document.getElementById("email").value == "") || 
+    (document.getElementById("object").value == "") || 
+    (document.getElementById("message").value == "")) {
+        alert("Compila i campi obbligatori")
+    } else {
+        var params = {
+            from_name : document.getElementById("full-name").value,
+            email_id : document.getElementById("email").value,
+            object : document.getElementById("object").value,
+            message : document.getElementById("message").value
+        }
+    
+        emailjs.send("service_tovtr19", "template_s0f2f17", params)
+            .then(alert("Messaggio inviato correttamente"));
+    } 
 }
